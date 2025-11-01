77001471ea418a4e6e887902e07fdaac37529e64
SoudagarLondhe <saudagar1209@gmail.com>
2025-11-01 14:04:29 -0400

    Routing added

---
+++ frontend/src/Chatbot.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function Chatbot() {
+
+  return (
+    <>
+     <h1>Inside Chatbot</h1>
+    </>
+  )
+}
+
+export default Chatbot
---
+++ frontend/src/Login.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function Login() {
+
+  return (
+    <>
+     <h1>Inside Application</h1>
+    </>
+  )
+}
+
+export default Login
